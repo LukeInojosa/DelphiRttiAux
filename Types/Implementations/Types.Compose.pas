b0d4b8e0e7efa7342c4b26365860a99120be0216
LukeInojosa <lucasinojosa81@gmail.com>
2026-09-09 22:24:11 -0300

    Compose types

--- Types/Implementations/Types.Compose.pas
+++ Types/Implementations/Types.Compose.pas
@@ -7,8 +7,21 @@ uses
 
 type
   RIntervaloCodigo = record
-    Intervalo: RIntervalo;
-    Codigo: RCodigo;
+  private
+    FIntervalo: RIntervalo;
+    FCodigo: RCodigo;
+
+    function getCodigo: Integer;
+    function getDataFinal: TDateTime;
+    function getDataInicial: TDateTime;
+    procedure setCodigo(const Value: Integer);
+    procedure setDataFinal(const Value: TDateTime);
+    procedure setDataInicial(const Value: TDateTime);
+  public
+
+    property DataInicial: TDateTime read getDataInicial write setDataInicial;
+    property DataFinal: TDateTime read getDataFinal write setDataFinal;
+    property Codigo: Integer read getCodigo write setCodigo;
 
     class operator Explicit(AString: String): RIntervaloCodigo;
     function ToString: String;
@@ -20,14 +33,44 @@ implementation
 
 class operator RIntervaloCodigo.Explicit(AString: String): RIntervaloCodigo;
 begin
-  Result.Intervalo := RIntervalo(AString);
-  Result.Codigo := RCodigo(AString);
+  Result.FIntervalo := RIntervalo(AString);
+  Result.FCodigo := RCodigo(AString);
+end;
+
+function RIntervaloCodigo.getCodigo: Integer;
+begin
+  Result := Self.FCodigo.Data.codigo;
+end;
+
+function RIntervaloCodigo.getDataFinal: TDateTime;
+begin
+  Result := Self.FIntervalo.Data.dataFinal;
+end;
+
+function RIntervaloCodigo.getDataInicial: TDateTime;
+begin
+  Result := Self.FIntervalo.Data.dataFinal;
+end;
+
+procedure RIntervaloCodigo.setCodigo(const Value: Integer);
+begin
+  Self.FCodigo.Data.codigo := Value;
+end;
+
+procedure RIntervaloCodigo.setDataFinal(const Value: TDateTime);
+begin
+  Self.FIntervalo.Data.dataFinal := Value;
+end;
+
+procedure RIntervaloCodigo.setDataInicial(const Value: TDateTime);
+begin
+  Self.FIntervalo.Data.dataFinal := Value;
 end;
 
 function RIntervaloCodigo.ToString: String;
 begin
-  Result := Self.Intervalo.ToString;
-  Result := Result + Self.Codigo.ToString;
+  Result := Self.FIntervalo.ToString;
+  Result := Result + Self.FCodigo.ToString;
 end;
 
 end.
